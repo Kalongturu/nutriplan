2339dc3ff6d1997f3134543888a70d268004bd01
Kalongturu <accoungaming12@gmail.com>
2026-03-18 17:42:01 +0700

    Ubah komponen navbar responsive

--- index.php
+++ index.php
@@ -7,53 +7,56 @@
   <title>NutriPlan - Homepage</title>
   <!-- Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
   <link rel="stylesheet" href="style/style.css">
 </head>
 
 <body>
   <nav class="navbar navbar-expand-lg navbar-light bg-transparent fixed-top py-3">
     <div class="container-fluid">
-      <div class="col-2" id="navbarSupportedContent">
+      <!-- Logo -->
+      <div class="col-2">
         <a class="navbar-brand" href="#">
           <img src="img/logo.png" alt="Logo NutriPlan" width="37px" height="37px">
           <span class="nutri">Nutri</span><span class="plan">Plan</span>
         </a>
       </div>
-
-      <button class="navbar-toggler me-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="#navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <!-- Button navbar responsive -->
+      <button class="navbar-toggler me-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse col-10" id="navbarSupportedContent">
-        <div class="d-flex justify-content-center flex-grow-1">
+      <!-- Elemen navbar -->
+      <div class="collapse navbar-collapse pe-3" id="navbarSupportedContent">
+        <div class="d-flex justify-content-center align-items-center flex-grow-1">
           <ul class="navbar-nav mb-2 mb-lg-0">
             <li class="nav-item tex">
-              <a class="nav-link ijo rounded-pill text-white fw-bold mx-2" aria-current="page" href="#">Home</a>
+              <a class="w-100 text-center nav-link ijo rounded-pill text-white fw-bold mx-2" aria-current="page" href="#">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link oren rounded-pill text-white fw-bold mx-2" href="#">How It Works</a>
+              <a class="w-100 text-center nav-link oren rounded-pill text-white fw-bold mx-2" href="#">How It Works</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link ijo rounded-pill text-white fw-bold mx-2" href="#">Recipes</a>
+              <a class="w-100 text-center nav-link ijo rounded-pill text-white fw-bold mx-2" href="#">Recipes</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link oren rounded-pill text-white fw-bold mx-2" href="#">Callculator</a>
+              <a class="w-100 text-center nav-link oren rounded-pill text-white fw-bold mx-2" href="#">Callculator</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link ijo rounded-pill text-white fw-bold mx-2" href="#">Planner</a>
+              <a class="w-100 text-center nav-link ijo rounded-pill text-white fw-bold mx-2" href="#">Planner</a>
             </li>
           </ul>
 
         </div>
         <ul class="navbar-nav mb-2 ms-auto mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link oren rounded-pill text-white fw-bold login" href="#">Log In/Sign Up</a>
+            <a class="text-center w-100 nav-link oren rounded-pill text-white fw-bold login" href="#">Log In/Sign Up</a>
           </li>
         </ul>
-      </div>
 
+      </div>
+    </div>
   </nav>
   <main> @yield(section:"content")
+    <!-- Isi konten -->
     <div class="container-fluid min-vh-100 lat-blkg px-4 konten" width="100%">
       <div class="row min-vh-100 align-items-center ps-5">
         <div class="col-md-6 flex-column">
@@ -69,6 +72,7 @@
             <a href="#" class="align-items-center btn oren text-white rounded-pill fw-bold ms-4" width="33%">Explore Recipes</a>
           </div>
         </div>
+        <!-- Elemen gambar -->
         <div class="container col-md-6">
           <div class="row text-center">
             <div class="col-md-2 position-relative border">
@@ -100,7 +104,8 @@
   </main>
 
   <!-- Bootstrap -->
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
 
 </html>
